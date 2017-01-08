04e4694e6e5537a3888bf806a0ef8e228a35c9d5
csDaniel.Mac <ofarreld@oregonstate.edu>
2017-01-08 17:17:07 +0900

    added a tiny file to ensure mac reformat was a success.

---
+++ macTest.py
@@ -0,0 +1,10 @@
+#!/usr/python
+
+def hello():
+	print("testing a print statement from within the mac book as opposed to PC.")
+hello()
+
+def echo():
+	x = raw_input("Enter text: ")
+	print ("{}".format(x))
+echo()
